f6c055ef7f7af63cbd012b00bc2f3fd433f69704
nick-neely <neelynickolas@gmail.com>
2024-12-01 14:58:44 -0600

    Add a separate resume page utilizing the resume.json, link it in the terminal

---
+++ utils/resumeParser.ts
@@ -0,0 +1,12 @@
+
+import { ResumeSchema } from "@/types/schema";
+import resumeData from "../config/resume.json";
+
+export const getResumeData = () => {
+  try {
+    return ResumeSchema.parse(resumeData);
+  } catch (error) {
+    console.error("Error parsing resume data:", error);
+    return null;
+  }
+};
